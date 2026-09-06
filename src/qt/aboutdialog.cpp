c940a97bda0ed391463c63e238ac77e919d251f1
Xnuva Blockchain Project <4691202+Xnuva-Blockchain-Project@users.noreply.github.com>
2026-09-06 06:28:05 +0100

    Stage 7D: correct maintained TaoCoin attribution wording

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -17,7 +17,7 @@ AboutDialog::AboutDialog(QWidget *parent) :
     // Set current copyright year
     ui->copyrightLabel->setText(
         tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin developers") + QString("<br>") +
-        tr("Copyright") + QString(" &copy; 2017 ") + tr("The TaoCoin developers") + QString("<br>") +
+        tr("Original TaoCoin legacy chain release: 2017") + QString("<br>") +
         tr("Maintained %1 by the Xnuva Blockchain Project").arg(MAINTENANCE_YEAR) + QString("<br>") +
         QString("<a href=\"https://europazeus.org\">europazeus.org</a>"));
     ui->copyrightLabel->setOpenExternalLinks(true);
